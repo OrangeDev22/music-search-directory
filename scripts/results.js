593801b733f703aecf2954866c4ba61150bda938
OrangeDev22 <OrangeDev8@gmail.com>
2021-02-25 14:56:06 -0600

    songs styling and navbar on results page

--- scripts/results.js
+++ scripts/results.js
@@ -4,10 +4,16 @@ const urlParams = new URLSearchParams(queryString);
 const baseUrl = "https://itunes.apple.com/search?term=";
 const searchText = urlParams.get("search");
 const results = [];
-let resultShow = document.querySelector(".result-show");
+const titlte_div = document.getElementById("title");
+let resultShow = document.querySelector(".result_show");
 let loading = true;
 let title = document.createElement("h3");
-body.prepend(title);
+let songs = document.getElementsByClassName("song");
+let currentSong;
+
+console.log(songs);
+
+titlte_div.appendChild(title);
 
 const fetchData = async () => {
   const url = `${baseUrl}${searchText}&attribute=mixTerm&media=music`;
@@ -21,7 +27,7 @@ const fetchData = async () => {
       results.forEach((result) => {
         let show = `
         <div class = "song">
-            <img src="${result.artworkUrl100}" value= "${result.previewUrl}" alt="">
+            <div class="image_wrapper"> <img src="${result.artworkUrl100}" value= "${result.previewUrl}" alt=""> </div>
             <h3> ${result.artistName} </h3>
             <h2> ${result.trackName} </h2>
             <audio controls class = "play">
